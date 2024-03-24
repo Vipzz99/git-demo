79c4d2aef166985cafca779b10e5e07ce5ce7cec
Vipzz99 <ajmeravipul1008@gmail.com>
2024-03-24 16:30:57 +0530

    Added sum

--- demo.cpp
+++ demo.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+	int a=10;
+	int b=20;
+	int sum= a+b;
+	cout << "Sum is : "<< sum << endl;
 	cout << "Hello Git!" << endl;
 	cout << "A new line in our file" << endl;
 	return 0;
