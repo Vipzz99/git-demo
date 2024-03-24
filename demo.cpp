5bef50b952a19beb3606ca170c3efafc37673072
Vipzz99 <ajmeravipul1008@gmail.com>
2024-03-24 16:51:05 +0530

    added subtraction code

--- demo.cpp
+++ demo.cpp
@@ -7,7 +7,9 @@ int main()
 	int a=10;
 	int b=20;
 	int sum= a+b;
+	int sub= a-b;
 	cout << "Sum is : "<< sum << endl;
+	cout << "Diff is : "<< sub << endl;
 	cout << "Hello Git!" << endl;
 	cout << "A new line in our file" << endl;
 	return 0;
